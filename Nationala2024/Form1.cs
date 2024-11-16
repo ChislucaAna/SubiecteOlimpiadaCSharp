eba5859fb5dfe6c6e67a478a514cd6ca6703798c
Ana <ana.chisluca.code@gmail.com>
2024-11-16 07:39:42 +0200

    DB creation

--- Nationala2024/Form1.cs
+++ Nationala2024/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace Nationala2024
 {
@@ -15,6 +17,43 @@ namespace Nationala2024
         public Form1()
         {
             InitializeComponent();
+            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\CosmosDB.mdf;Integrated Security=True");
+        }
+
+        SqlConnection con;
+        SqlCommand cmd;
+        StreamReader reader;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            reader = new StreamReader("Utilizatori.txt");
+            string line;
+            while ((line=reader.ReadLine())!=null)
+            {
+                string[] fields = line.Split(';');
+                DateTime datetime = DateTime.ParseExact(fields[4], "MM.dd.yyyy",null);
+                string[] bucati = datetime.ToString().Split(' ');
+
+                con.Open();
+                cmd = new SqlCommand(String.Format("INSERT INTO Utilizatori VALUES('{0}','{1}','{2}','{3}','{4}');",
+                    fields[0], fields[1], fields[2], fields[3], bucati[0]), con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+            reader = new StreamReader("Inregistrari.txt");
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] fields = line.Split(';');
+                DateTime datetime = DateTime.ParseExact(fields[2], "dd/MM/yyyy", null);
+                string[] bucati = datetime.ToString().Split(' ');
+
+                con.Open();
+                cmd = new SqlCommand(String.Format("INSERT INTO Inregistrari VALUES('{0}','{1}',{2},{3});",
+                    fields[0], bucati[0], fields[1], fields[3]), con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
         }
     }
 }
