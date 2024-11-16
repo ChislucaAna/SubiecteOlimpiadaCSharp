e026469e9d0c0855251d6c9aea7fc6b59d7c17fe
Ana <ana.chisluca.code@gmail.com>
2024-11-16 08:40:43 +0200

    auth

---
+++ Nationala2024/Cosmos-Imagini.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Nationala2024
+{
+    partial class Cosmos_Imagini
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Cosmos_Imagini
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Cosmos_Imagini";
+            this.Text = "Cosmos- Imagini";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
